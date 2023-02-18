0715351c2ce27289b7a5a0e8be2db862578051ab
alme7airbi93 <alme7airbi93@live.com>
2023-02-18 13:49:08 +0400

    Motion detection function Frame writing fixed

--- camera.py
+++ camera.py
@@ -3,57 +3,93 @@ import time
 import database
 from datetime import datetime
 import os
-
+from motion import motionDetection
+from config import cfg
 
 def gen_frames(cam_id):
     cameras = database.CCTV_DB().get_CCTV_List()
     settings = database.SETTINGS_DB().get_settings_Dict()
     storagePath = str(settings['filePath'] + '\\')
+    fourcc = cv2.VideoWriter_fourcc(*cfg.video.Encoder)
     start_time = time.time()
+    frameCmp = None
+    frameCount = 0 
+    KPS = cfg.video.Fps # desired frame per second
 
     for cctv in cameras:
+        
         if int(cctv.doc_id) == int(cam_id):
             cap = cv2.VideoCapture(f'{cctv["protocol"]}://{cctv["username"]}:{cctv["password"]}@{cctv["ip"]}:{cctv["port"]}')
+            time.sleep(0.25)
             print("Started  @  : " + str(datetime.now().strftime("%Y-%m-%d-%H%M%S")))
+            
+            fps = round(cap.get(cv2.CAP_PROP_FPS))
+            hop = round(fps / KPS)
 
-            frame_width = int(cap.get(3))
-            frame_height = int(cap.get(4))
-            fileName = storagePath + "\\" + str(cctv.doc_id) + "\\" + str(
-                datetime.now().strftime("%Y-%m-%d-%H%M%S")) + ".avi"
+            frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))    #frame_width = int(cap.get(3))
+            frame_height= int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))   #frame_height= int(cap.get(4))
+            if frame_width <10 or frame_height < 10:  # for very less resolution video, generate warning.
+                print('Camera Errro, exiting...', frame_width, frame_height)
+                break
+            
+            dimensions = (int(frame_width * cfg.video.Scale), int(frame_height * cfg.video.Scale) )
+            
+            fileName = storagePath + "\\" + str(cctv.doc_id) + "\\" + str(datetime.now().strftime("%Y-%m-%d-%H%M%S")) + ".avi"
+            
             if not os.path.isdir(storagePath + "\\" + str(cctv.doc_id)):
                 os.makedirs(storagePath + "\\" + str(cctv.doc_id))
-            out = cv2.VideoWriter(fileName, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10,
-                                  (frame_width, frame_height))
+            
+            out = cv2.VideoWriter(fileName, fourcc, cfg.video.Fps, dimensions)
+
             recordDuration = int(settings['vidDuration']) * 60
             print("Record Duration : " + str(recordDuration))
-            while True:
+
+            while cap.isOpened():   # chaanged fron True by TJ
                 now = time.time()
                 success, frame = cap.read()  # read the camera frame
                 elapsed = round(now - start_time)
-
+                
                 if not success:
                     cap.release()
+                    #Tries to reset !!
                     cap = cv2.VideoCapture(f'{cctv["protocol"]}://{cctv["username"]}:{cctv["password"]}@{cctv["ip"]}:{cctv["port"]}')
                     print("Stopped @  : " + str(datetime.now().strftime("%Y-%m-%d-%H%M%S")))
+                
                 else:
+                    frameCount += 1
+                    the_frame = frame.copy()
+                    if cfg.video.Scale < 1.0:   # rescale if rescale factor is set (lower than 1)
+                        the_frame = cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)  # rescaling using opencv
+                                           
                     try:
                         if elapsed < recordDuration:
-                            out.write(frame)
-                            # time.sleep(0.25)
+                            if frameCount % 5 == 0:  # detect motion every 5th frame
+                                the_frame, motion, frameCmp = motionDetection(frameCmp, the_frame)  # call motion detection function
+                            
+                            if (cfg.video.Show):
+                                print('motion detected:', motion >0)
+                            
+                            if frameCount % hop == 0:   # only write the selected frame
+                                if motion > 0:
+                                    out.write(the_frame) #TJ write after all the processing,
                         else:
                             out.release()
-                            print("Record Saved : " + fileName)
+                            print("Record Saved : " + fileName, elapsed)
+                            
                             print("Starting new ...")
                             fileName = storagePath + "\\" + str(cctv.doc_id) + "\\" + str(
                                 datetime.now().strftime("%Y-%m-%d-%H%M%S")) + ".avi"
-                            out = cv2.VideoWriter(fileName, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10,
-                                                  (frame_width, frame_height))
+                            #initalizing file handle
+                            out = cv2.VideoWriter(fileName, fourcc, cfg.video.Fps, dimensions)
+                            #initializing timer                       
                             start_time = time.time()
-
+                            frameCount = 0
+                      
                     except Exception as e:
                         print("Error Rised : "+ str(e))
 
-                    ret, buffer = cv2.imencode('.jpg', frame)
-                    frame = buffer.tobytes()
-                    yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                    ret, buffer = cv2.imencode('.jpg', the_frame)   # the frame can be replaced with rescaled_frame if related to disk storage
+                    vframe = buffer.tobytes()
+                    yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + vframe + b'\r\n')
+    
     return "Camera not found"
---
+++ motion.py
@@ -0,0 +1,94 @@
+import cv2
+import numpy as np
+from config import cfg
+#---------------------------------------------------------------------------------------------------------------
+# This function allows us to create a descending sorted list of contour areas., used in 'motionDetection' below
+def contour_area(contours):
+    cnt_area = []  # create an empty list
+    for i in range(0,len(contours),1): # loop through all the contours
+        # for each contour, use OpenCV to calculate the area of the contour
+        cnt_area.append(cv2.contourArea(contours[i]))
+
+    # Sort our list of contour areas in descending order
+    list.sort(cnt_area, reverse=True)
+    return cnt_area
+#---------------------------------------------------------------------------------------------------
+def show_windows(title, image):
+    if cfg.video.Flag:
+        cv2.imshow(title, image)
+        key=cv2.waitKey(1) & 0xFF
+        if key==ord('q'):
+            cv2.destroyWindow(title)
+            cfg.video.Flag = not (cfg.video.Flag)
+#---------------------------------------------------------------------------------------------------
+# import cascade file for recognition
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+import imutils
+import numpy as np
+
+conf = dict({   # developmental work
+    "show_video":   cfg.video.Show,
+    "delta_thresh": cfg.motion.senseThreshold,   # defaul 6
+    "min_area":     cfg.motion.senseArea   # default = 600
+})
+
+def motionDetection(avg, fr):
+    
+    #frame = np.array(fr)
+    frame = fr.copy()
+    # convert the frame to grayscale, and blur it
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # if the average frame is None, initialize it
+    if avg is None:
+        print("[INFO] starting background model...")
+        avg = gray.copy().astype("float")
+
+    # accumulate the weighted average between the current frame and previous frames,
+    # then compute the difference between the current frame and running average
+    cv2.accumulateWeighted(gray, avg, 0.6)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=3)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:15]
+
+    img = frame
+    mot = 0
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if (cv2.contourArea(c) < conf["min_area"]):
+            continue
+        # compute the bounding box for the contour, draw it on the frame
+        #print(frameDelta.shape)
+        (x, y, w, h) = cv2.boundingRect(c)
+        #if (x>350 and (x+w)<580 and y>50 and (y+h)<110):
+        if (x>cfg.motion.maskLeft and (x+w)<cfg.motion.maskRight and y>cfg.motion.maskUp and (y+h)<cfg.motion.maskDwon):
+            continue
+        #else:
+            print(x,y,x+w, y+h)
+        if cfg.motion.redBox:
+            img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        
+        if cfg.motion.blueBox:
+            rect = cv2.minAreaRect(c)   # get the min area rect
+            box  = cv2.boxPoints(rect)
+            box  = np.int0(box)          # convert all coordinates floating point values to int
+            img = cv2.drawContours(img, [box], 0, (255, 0, 0),1)  # draw a blue 'nghien' rectangle
+        
+            #print(cv2.contourArea(c),w,h)
+        mot += 1
+
+    if conf["show_video"]:
+        # display the security feed
+        cv2.imshow("Security Feed", img)
+        key = cv2.waitKey(1) & 0xFF
+        # if the `q` key is pressed, break from the lop
+        if key == ord("q"):
+            exit(0)
+    
+    return img, mot, avg
+#------------------------------------------------------------------------------------------------------------------------------------
